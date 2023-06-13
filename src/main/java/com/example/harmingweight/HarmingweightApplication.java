662c345baf7b13990d4b0a636db25cd9e44d7c5e
Ailin001019 <wi22b030@technikum-wien.at>
2023-06-13 11:56:16 +0200

    Initial commit

---
+++ src/main/java/com/example/harmingweight/HarmingweightApplication.java
@@ -0,0 +1,13 @@
+package com.example.harmingweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmingweightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmingweightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/harmingweight/HarmingweightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harmingweight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmingweightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
